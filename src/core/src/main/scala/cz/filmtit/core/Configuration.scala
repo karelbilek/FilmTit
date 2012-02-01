244ea05714f18b78defd7158b456cab5645b760e
Jo Daiber <jodaiber@gmx.de>
2012-02-01 19:22:25 +0100

    Added Configuration, updated heap sizes for server.

---
+++ src/core/src/main/scala/cz/filmtit/core/Configuration.scala
@@ -0,0 +1,22 @@
+package cz.filmtit.core
+
+/**
+ * @author Joachim Daiber
+ *
+ *
+ *
+ */
+
+object Configuration {
+
+  //Import:
+  val alignedChunkFolder = "/Users/jodaiber/Desktop/LCT/LCT W11:12/FilmTit/data/parallel/utf8"
+  val subtitleMovieMapping = "/Users/jodaiber/Dropbox/FilmTit/Examples, experiments/opensubtitles stats/export_final.txt"
+
+  //Database:
+  val dbConnector = "jdbc:postgresql://localhost/filmtit"
+  val dbUser = "postgres"
+  val dbPassword = "postgres"
+
+
+}
