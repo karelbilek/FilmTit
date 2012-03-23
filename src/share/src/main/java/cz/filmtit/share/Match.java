b0ce2cf47da72063d131ea6a3ebb20a262fd740e
Jindrich Libovicky <jlibovicky@gmail.com>
2012-03-23 14:00:16 +0100

    Attempt to solve the dependencies problem.

---
+++ src/share/src/main/java/cz/filmtit/share/Match.java
@@ -0,0 +1,8 @@
+package cz.filmtit.share;
+
+import java.util.List;
+
+public class Match {
+    private String text;
+    private List<Translation> translations;
+}
---
+++ src/share/src/main/java/cz/filmtit/share/Translation.java
@@ -0,0 +1,6 @@
+package cz.filmtit.share;
+
+public class Translation {
+    public String text = null;
+    public double score = Double.MIN_VALUE;
+}
