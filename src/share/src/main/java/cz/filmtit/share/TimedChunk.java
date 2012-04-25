91ae267bf2d24dfe71598ec2d541c13280090fe3
jlibovicky <jlibovicky@gmail.com>
2012-04-25 22:07:34 +0200

    Adding missing classes I forgot yesterday.

--- src/share/src/main/java/cz/filmtit/share/TimedChunk.java
+++ src/share/src/main/java/cz/filmtit/share/TimedChunk.java
@@ -2,15 +2,39 @@ package cz.filmtit.share;
 
 import java.io.Serializable;
 
-
-/**
- * @author Joachim Daiber
- */
 public class TimedChunk extends Chunk implements Serializable {
+    private String startTime;
+    private String endTime;
+    private int partNumber;
 
-	public TimedChunk(String surfaceform) {
-		super(surfaceform);
-		// TODO Auto-generated constructor stub
-	}
+    public TimedChunk(String startTime, String endTime, int partNumber, String text) {
+        super(text);
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.partNumber = partNumber;
+    }
 
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(int partNumber) {
+        this.partNumber = partNumber;
+    }
 }
