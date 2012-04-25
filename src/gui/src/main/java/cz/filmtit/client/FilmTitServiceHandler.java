17a1a585dea113a3851437847a9da615a7387235
Ruda Rosa <rur@seznam.cz>
2012-04-25 17:19:16 +0200

    a typo

--- src/gui/src/main/java/cz/filmtit/client/FilmTitServiceHandler.java
+++ src/gui/src/main/java/cz/filmtit/client/FilmTitServiceHandler.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
 
 import cz.filmtit.share.TimedChunk;
 import cz.filmtit.share.TranslationResult;
@@ -17,29 +18,33 @@ public class FilmTitServiceHandler {
 	
 	// TODO
 	Button btnTranslate = new Button("Translate");
-	btnTranslate.addClickHandler(new ClickHandler() {
-		public void onClick(ClickEvent event) {
-			// handling todleto
-			if (filmTitSvc == null) {
-				filmTitSvc = GWT.create(FilmTitService.class);
+	translation = new Label("");
+
+	public void suggestions(TimedChunk chunk) {
+		btnTranslate.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				// handling todleto
+				if (filmTitSvc == null) {
+					filmTitSvc = GWT.create(FilmTitService.class);
+				}
+				
+				AsyncCallback<TranslationResult> callback = new AsyncCallback<TranslationResult>() {
+					
+					public void onSuccess(TranslationResult result) {
+						translation.setText(result.getUserTranslation());
+					}
+					
+					public void onFailure(Throwable caught) {
+						Window.alert(caught.getLocalizedMessage());
+					}
+				};
+				
+				TimedChunk chunk = new TimedChunk(txtbxText.getText());
+				filmTitSvc.suggestions(chunk, callback);
+				
 			}
-			
-			AsyncCallback<TranslationResult> callback = new AsyncCallback<TranslationResult>() {
-				
-				public void onSuccess(TranslationResult result) {
-					translation.setText(result.getUserTranslation());
-				}
-				
-				public void onFailure(Throwable caught) {
-					Window.alert(caught.getLocalizedMessage());
-				}
-			};
-			
-			TimedChunk chunk = new TimedChunk(txtbxText.getText());
-			filmTitSvc.suggestions(chunk, callback);
-			
-		}
-	});
+		});
+	}
 	
 	
 }
