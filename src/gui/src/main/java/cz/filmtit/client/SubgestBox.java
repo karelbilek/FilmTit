c26203d558189c4b5ad226f6f65f73b6cf78d510
Honza Vacl <omikro2n@gmail.com>
2012-05-04 12:42:28 +0200

    more gui update, added a handler

--- src/gui/src/main/java/cz/filmtit/client/SubgestBox.java
+++ src/gui/src/main/java/cz/filmtit/client/SubgestBox.java
@@ -2,22 +2,24 @@ package cz.filmtit.client;
 
 import java.util.List;
 
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.event.dom.client.KeyPressEvent;
-import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 import cz.filmtit.share.TranslationResult;
 import cz.filmtit.share.TranslationPair;
@@ -35,12 +37,12 @@ public class SubgestBox extends TextBox {
 	private int id;
 	private TranslationResult translationResult;
 	private Widget suggestionWidget;
-	private RootPanel rootPanel;
+	private Gui gui;
 	
-	public SubgestBox(int id, TranslationResult translationResult, RootPanel rootPanel) {
+	public SubgestBox(int id, TranslationResult translationResult, Gui gui) {
 		this.id = id;
 		this.translationResult = translationResult;
-		
+		this.gui = gui;
 	}
 	
 	public int getId() {
@@ -59,19 +61,64 @@ public class SubgestBox extends TextBox {
 		return suggestionWidget;
 	}
 	
+	
+	private class SuggestionCell extends AbstractCell<TranslationPair> {
+		@Override
+		public void render(com.google.gwt.cell.client.Cell.Context context,
+				TranslationPair value, SafeHtmlBuilder sb) {
+			if (value == null) {
+				return;
+			}
+			sb.appendEscapedLines(value.getStringL2() + "\n(\"" + value.getStringL1() + "\")" + "\n");
+		}		
+	}
+	
 	public void showSuggestions() {
-		AbsolutePanel parentPanel = (AbsolutePanel)this.getParent();
-		List<TranslationPair> suggestions = this.getSuggestions();
-		String suggestion = "";
-		for (TranslationPair suggestiontp : suggestions) {
-			suggestion += suggestiontp.getStringL2() + "\n(\"" + suggestiontp.getStringL1() + "\")" + "\n";
-		}
+		// creating the suggestions pop-up panel:
+		FlowPanel suggestPanel = new FlowPanel();
 		
-		Label lblSuggestions = new HTML(new SafeHtmlBuilder().appendEscapedLines(suggestion).toSafeHtml());
-		lblSuggestions.setStylePrimaryName("suggestionsPopup");
-		int sugleft = parentPanel.getAbsoluteLeft() + parentPanel.getWidgetLeft(this) + 8;
-		int sugtop  = parentPanel.getAbsoluteTop()  + parentPanel.getWidgetTop(this) + 25;
-		parentPanel.add(lblSuggestions, sugleft, sugtop);
-		this.setSuggestionWidget(lblSuggestions);
+		
+		CellList<TranslationPair> cellList = new CellList<TranslationPair>(new SuggestionCell());
+		cellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		// Add a selection model to handle user selection.
+		final SingleSelectionModel<TranslationPair> selectionModel = new SingleSelectionModel<TranslationPair>();
+		cellList.setSelectionModel(selectionModel);
+		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+			public void onSelectionChange(SelectionChangeEvent event) {
+				TranslationPair selected = selectionModel.getSelectedObject();
+				if (selected != null) {
+					Window.alert("selected suggestion: " + selected.getChunkL2().getSurfaceForm());
+				}
+			}
+		});
+		cellList.setRowData(this.getSuggestions());
+		suggestPanel.add(cellList);
+		
+		/*
+		ClickHandler suggestionClickHandler = new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Label suglabel = (Label) event.getSource();
+				gui.submitUserTranslation(transresult)
+			}
+		};
+		List<TranslationPair> suggestions = this.getSuggestions();
+		for (TranslationPair suggestiontp : suggestions) {
+			String suggestion = suggestiontp.getStringL2() + "\n(\"" + suggestiontp.getStringL1() + "\")" + "\n";
+			Label lblSuggestions = new HTML(new SafeHtmlBuilder().appendEscapedLines(suggestion).toSafeHtml());
+			lblSuggestions.setStylePrimaryName("suggestion");
+			suggestPanel.add(lblSuggestions);
+		}
+		*/
+		suggestPanel.setStylePrimaryName("suggestionsPopup");
+		
+		
+		//AbsolutePanel parentPanel = (AbsolutePanel) this.getParent();
+		//int sugleft = parentPanel.getAbsoluteLeft() + parentPanel.getWidgetLeft(this) + 8;
+		//int sugtop  = parentPanel.getAbsoluteTop()  + parentPanel.getWidgetTop(this) + 25;
+		int sugleft = this.getAbsoluteLeft() - this.gui.rootPanel.getAbsoluteLeft() + 8;
+		int sugtop  = this.getAbsoluteTop()  - this.gui.rootPanel.getAbsoluteTop() + 35;
+		this.gui.rootPanel.add(suggestPanel, sugleft, sugtop);
+		this.setSuggestionWidget(suggestPanel);
 	}
 }
