7ae52ed6d87db314e3c44259620e5161c503fbb6
Karel Bilek <bilek7am@u-pl12.ms.mff.cuni.cz>
2012-05-07 15:59:31 +0200

    include dependencies with interactive scala (mostly for playing with the classes)

--- src/interactive_scala_with_filmtit_classes.sh
+++ src/interactive_scala_with_filmtit_classes.sh
@@ -1,1 +1,3 @@
-scala -cp ./core/target/classes:./share/target/classes:./eval/target/classes:./userspace/target/classes
+#you first need to run mvn dependency:copy-dependencies to dependency to work
+#(there is probably an easier way :))
+scala -cp ./core/target/classes:./share/target/classes:./eval/target/classes:./userspace/target/classes:./core/target/dependency/*:./share/target/dependency/*:./eval/target/dependency/*:./userspace/target/dependency/*
