505904320c2bb1dbfe7216404e936a1ab8f84423
Karel Bilek <bilek7am@u-pl12.ms.mff.cuni.cz>
2012-05-17 06:06:02 +0200

    it has to be serializable too for database

--- src/share/src/main/java/cz/filmtit/share/MediaSource.java
+++ src/share/src/main/java/cz/filmtit/share/MediaSource.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
  *
  * @author Joachim Daiber
  */
-public class MediaSource implements com.google.gwt.user.client.rpc.IsSerializable {
+public class MediaSource implements com.google.gwt.user.client.rpc.IsSerializable, Serializable {
 
 	private static final long serialVersionUID = 5899275001958847885L;
 
