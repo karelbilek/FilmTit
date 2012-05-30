520de08a24aa98d59dcbb99e5672c4da520f26fb
jlibovicky <jlibovicky@gmail.com>
2012-05-30 14:10:00 +0200

    Repushing MockFilmtitBackendServer which I accidentally deleted by the last merge.

---
+++ src/userspace/src/test/java/cz/filmtit/userspace/tests/MockFilmTitBackendServer.java
@@ -0,0 +1,20 @@
+package cz.filmtit.userspace.tests;
+
+import cz.filmtit.core.Configuration;
+import cz.filmtit.core.tests.TestUtil;
+import cz.filmtit.userspace.FilmTitBackendServer;
+import java.io.File;
+
+/**
+ * Keeps all the functionality of the FilmtitBackendServer, except loading the translation memory which is
+ * switched to the in memory database.
+ *
+ * @author Jindřich Libovický
+*/
+
+public class MockFilmTitBackendServer extends FilmTitBackendServer {
+    @Override
+    protected void loadTranslationMemory() {
+        TM =  TestUtil.createTMWithDummyContent(new Configuration(new File("configuration.xml")));
+    }
+}
