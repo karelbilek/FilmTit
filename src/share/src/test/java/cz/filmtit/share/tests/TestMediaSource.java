57037d2fd0b0b4b572c039675c4ae6c1d32ba43d
jlibovicky <jlibovicky@gmail.com>
2012-05-31 16:35:54 +0200

    Fixing path to share tests and pom file.

---
+++ src/share/src/test/java/cz/filmtit/share/tests/TestMediaSource.java
@@ -0,0 +1,34 @@
+package cz.filmtit.share.tests;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: josef.cech
+ * Date: 17.5.12
+ * Time: 6:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+import cz.filmtit.share.MediaSource;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TestMediaSource {
+    private static MediaSource source;
+    @BeforeClass
+    public static void initTimedChunk(){
+     source = new MediaSource("TestMovie","2012","akční, komedie, thriller");
+    }
+
+
+
+    @Test
+    public void testChunkGetters(){
+        assertEquals("TestMovie",source.getTitle());
+        assertEquals("2012",source.getYear());
+        assertEquals(3,source.getGenres().size());
+        assertEquals(true,source.getGenres().contains("komedie"));
+    }
+}
