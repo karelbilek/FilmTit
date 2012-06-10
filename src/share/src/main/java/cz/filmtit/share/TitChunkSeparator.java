19768ace2c3e27653be4329e6c95f40eb819ea93
bilek7am <kb@karelbilek.com>
2012-06-10 04:37:45 +0200

    forgot to add tokenizers to separator

--- src/share/src/main/java/cz/filmtit/share/TitChunkSeparator.java
+++ src/share/src/main/java/cz/filmtit/share/TitChunkSeparator.java
@@ -5,20 +5,35 @@ import com.google.gwt.regexp.shared.SplitResult;
 
 import java.util.ArrayList;
 import java.util.List;
+import cz.filmtit.share.tokenizers.*;
 
 public class TitChunkSeparator {
+
+    public static CzechSentenceTokenizer czechTokenizer = new CzechSentenceTokenizer();
+    public static EnglishSentenceTokenizer englishTokenizer = new EnglishSentenceTokenizer();
 
 	public static final String SUBLINE_SEPARATOR_OUT = " | ";
 	
 	public static final RegExp indirectSplitter = RegExp.compile(" *\\| *");
-	public static final RegExp dialogSegmenter = RegExp.compile("^ ?- ?[a-zA-Z0-9]");
-	
+	public static final RegExp dialogSegmenter = RegExp.compile("^ ?- ?.");
+    public static final RegExp endOfLinePunct = RegExp.compile("[.?…!]");
+
 	// formatting tags - in srt e.g. "<i>", in sub e.g. "{Y:i}"
 	public static final RegExp formatTag = RegExp.compile("(<[^>]*>)|({[^}]*})", "g");  // the "{}" are here as literals
 
+    private static List<String> tokenizeByTokenizers(String tit, Language l) {
+       
+       if (l == Language.EN) {
+            return englishTokenizer.tokenize(tit);
+       } else {
+            return czechTokenizer.tokenize(tit);
+       }
+    }
 
-	public static List<String> separate(String tit) {
-		// remove formatting tags
+
+	public static List<String> separate(String tit, Language l) {
+        
+        // remove formatting tags
 		tit = formatTag.replace(tit, "");
 
 		SplitResult lines = indirectSplitter.split(tit);
@@ -30,15 +45,23 @@ public class TitChunkSeparator {
 
 			if (dialogSegmenter.test(line)) {
 				// is a dialog line -> splitting
-				resultChunks.add(intermediateChunk);
+				resultChunks.addAll(tokenizeByTokenizers(intermediateChunk, l));
 				intermediateChunk = line;
 			}
 			else {
 				intermediateChunk += SUBLINE_SEPARATOR_OUT + line;
-			}
+		        
+                //is punct at EOL -> splitting
+                if (endOfLinePunct.test(intermediateChunk)) {
+                    resultChunks.addAll(tokenizeByTokenizers(intermediateChunk,l));
+                    
+                }
+                intermediateChunk="";
+            }
 		}
-		resultChunks.add(intermediateChunk);
-		
+		if (!intermediateChunk.equals("")) {
+            resultChunks.addAll(tokenizeByTokenizers(intermediateChunk, l));
+		}
 		return resultChunks;
 	}
 
