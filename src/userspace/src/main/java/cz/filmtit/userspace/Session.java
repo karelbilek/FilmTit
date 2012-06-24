9efc090855844d4453e100c4d1658cdb1154c0f0
jlibovicky <jlibovicky@gmail.com>
2012-06-24 22:52:29 +0200

    Handling the active documents added to session termination.

--- src/userspace/src/main/java/cz/filmtit/userspace/Session.java
+++ src/userspace/src/main/java/cz/filmtit/userspace/Session.java
@@ -105,18 +105,18 @@ public class Session {
      * Terminates the session. Usually in the situation when the user open a new one.
      */
     private void terminate() {
-        
         org.hibernate.Session session = HibernateUtil.getSessionWithActiveTransaction();
-
         session.save(this);
 
-        // TODO: when proper users, save to database here
-        //user.saveToDatabase(session);
+        user.getActiveDocumentIDs().clear();
+
         for (USDocument activeDoc : activeDocuments.values()) {
             activeDoc.saveToDatabase(session);
+            user.getActiveDocumentIDs().add(activeDoc.getDatabaseId());
         }
-        HibernateUtil.closeAndCommitSession(session);
 
+        user.saveToDatabase(session);
+        HibernateUtil.closeAndCommitSession(session);
     }
 
     /**
