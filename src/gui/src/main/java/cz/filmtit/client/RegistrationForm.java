3a4e795d797a950312190f4aae54d7a5f9eb251d
Ruda Rosa <rur@seznam.cz>
2012-07-22 23:29:32 +0200

    adding registration form (not used yet)

---
+++ src/gui/src/main/java/cz/filmtit/client/RegistrationForm.java
@@ -0,0 +1,91 @@
+package cz.filmtit.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.HasText;
+import com.github.gwtbootstrap.client.ui.PasswordTextBox;
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.SubmitButton;
+import com.github.gwtbootstrap.client.ui.TextBox;
+
+public class RegistrationForm extends Composite {
+
+	private static RegistrationFormUiBinder uiBinder = GWT
+			.create(RegistrationFormUiBinder.class);
+
+	interface RegistrationFormUiBinder extends
+			UiBinder<Widget, RegistrationForm> {
+	}
+
+	public RegistrationForm() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@UiField
+	TextBox txtEmail;
+
+	@UiField
+	TextBox txtName;
+
+	@UiField
+    PasswordTextBox txtPassword;
+
+	@UiField
+    PasswordTextBox txtPasswordRepeat;
+
+	@UiField
+	Button btnRegister;
+	
+	@UiField
+	Button btnCancel;
+	
+    public String getEmail() {
+        return txtEmail.getText();
+    }
+
+    public String getName() {
+        return txtName.getText();
+    }
+
+    public String getPassword() {
+        return txtPassword.getText();
+    }
+    
+    
+    public boolean checkEmailValidity() {
+        String email = getEmail();
+        // TODO
+    	if (email.length() >= 5 && email.contains("@") && email.contains(".")) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    public boolean checkPassword() {
+        return checkPasswordsMatch() && checkPasswordStrength();
+    }
+    
+    public boolean checkPasswordsMatch() {
+        String password = txtPassword.getText();
+        String passwordRepeat = txtPasswordRepeat.getText();
+    	if (password.equals(passwordRepeat)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+	}
+    
+    public boolean checkPasswordStrength() {
+        String password = getPassword();
+        // TODO (but maybe keep that way)
+        if (password.length() >= 3) {
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+}
