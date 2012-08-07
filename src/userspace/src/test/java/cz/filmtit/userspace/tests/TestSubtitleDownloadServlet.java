ac5a0b4fed59ccc44add402b7a184df76b0bbdc0
jlibovicky <jlibovicky@gmail.com>
2012-08-07 23:28:12 +0200

    Test for deleting document.

--- src/userspace/src/test/java/cz/filmtit/userspace/tests/TestSubtitleDownloadServlet.java
+++ src/userspace/src/test/java/cz/filmtit/userspace/tests/TestSubtitleDownloadServlet.java
@@ -1,13 +1,29 @@
 package cz.filmtit.userspace.tests;
 
+import cz.filmtit.core.Configuration;
+import cz.filmtit.core.ConfigurationSingleton;
 import cz.filmtit.userspace.servlets.FilmTitBackendServer;
 import cz.filmtit.userspace.servlets.SubtitleDownloadServlet;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Jindřich Libovický
  */
 public class TestSubtitleDownloadServlet {
+    @BeforeClass
+    public static void setupConfiguration() {
+        Configuration configuration = new Configuration("configuration.xml");
+        ConfigurationSingleton.setConf(configuration);
+        MockHibernateUtil.changeUtilsInAllClasses();
+    }
+
+    @AfterClass
+    public static void clean() {
+        MockHibernateUtil.clearDatabase();
+    }
+
     @Test
     public void testGettingProperFile() {
         FilmTitBackendServer backendServer = new MockFilmTitBackendServer();
