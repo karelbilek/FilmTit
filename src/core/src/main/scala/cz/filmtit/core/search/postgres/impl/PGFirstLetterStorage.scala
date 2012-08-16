d63973b63e0aeb7ee6ae8df527c7fa9b90d4db06
Jo Daiber <jodaiber@gmx.de>
2012-08-17 01:59:55 +0200

    Only create firstletter function when indexing, drop if exists.

--- src/core/src/main/scala/cz/filmtit/core/search/postgres/impl/PGFirstLetterStorage.scala
+++ src/core/src/main/scala/cz/filmtit/core/search/postgres/impl/PGFirstLetterStorage.scala
@@ -21,46 +21,42 @@ class PGFirstLetterStorage(
   useInMemoryDB
 ) {
 
-  if (!connection.isReadOnly) {
-
-    //    DROP FUNCTION IF EXISTS firstletter(TEXT);
-    connection.createStatement().execute("""
-
-    CREATE FUNCTION firstletter(TEXT) RETURNS TEXT
-        AS $$
-
-    	DECLARE
-    		retVal TEXT[];
-    		tokens TEXT[];
-    	BEGIN
-    		tokens := array(SELECT token FROM ts_parse('default', $1) WHERE tokid != 12);
-        IF array_length(tokens, 1) IS NULL or array_length(tokens, 1) = 0 THEN
-          RETURN '';
-        END IF;
-
-    		FOR I IN 1..array_length(tokens, 1) LOOP
-    			IF array_length(tokens, 1) = 1 THEN
-    				retVal[I] := lower(tokens[I]);
-    			ELSIF array_length(tokens, 1) = 2 THEN
-    				retVal[I] := lower(substring(tokens[I] from 0 for 4));
-    			ELSIF array_length(tokens, 1) = 3 THEN
-    				retVal[I] := lower(substring(tokens[I] from 0 for 3));
-    			ELSE
-    				retVal[I] := lower(substring(tokens[I] from 0 for 2));
-    			END IF;
-    		END LOOP;
-
-    		RETURN array_to_string(retVal, ' ');
-    	END;
-        $$ LANGUAGE 'plpgsql' IMMUTABLE;
-""".stripMargin)
-
-  }
-
   override def warmup() {}
 
   override def reindex() {
     log.info("Reindexing...")
+
+    connection.createStatement().execute("""
+      DROP FUNCTION IF EXISTS firstletter(TEXT);
+      CREATE FUNCTION firstletter(TEXT) RETURNS TEXT
+          AS $$
+
+      	DECLARE
+      		retVal TEXT[];
+      		tokens TEXT[];
+      	BEGIN
+      		tokens := array(SELECT token FROM ts_parse('default', $1) WHERE tokid != 12);
+          IF array_length(tokens, 1) IS NULL or array_length(tokens, 1) = 0 THEN
+            RETURN '';
+          END IF;
+
+      		FOR I IN 1..array_length(tokens, 1) LOOP
+      			IF array_length(tokens, 1) = 1 THEN
+      				retVal[I] := lower(tokens[I]);
+      			ELSIF array_length(tokens, 1) = 2 THEN
+      				retVal[I] := lower(substring(tokens[I] from 0 for 4));
+      			ELSIF array_length(tokens, 1) = 3 THEN
+      				retVal[I] := lower(substring(tokens[I] from 0 for 3));
+      			ELSE
+      				retVal[I] := lower(substring(tokens[I] from 0 for 2));
+      			END IF;
+      		END LOOP;
+
+      		RETURN array_to_string(retVal, ' ');
+      	END;
+          $$ LANGUAGE 'plpgsql' IMMUTABLE;
+     """.stripMargin)
+
     connection.createStatement().execute(("DROP index IF EXISTS idx_firstletter_l1;" +
       "CREATE INDEX idx_firstletter_l1 ON %s (firstletter(chunk_l1), pair_count DESC);").format(pairTable))
     connection.createStatement().execute(("DROP index IF EXISTS idx_firstletter_l2;" +
