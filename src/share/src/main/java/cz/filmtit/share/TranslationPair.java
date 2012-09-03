09754317dbe42cea911bc1860c74fbfc65f4b3d3
Karel Bílek <kb@karelbilek.com>
2012-09-03 08:34:38 +0200

    our software is now FREE

--- src/share/src/main/java/cz/filmtit/share/TranslationPair.java
+++ src/share/src/main/java/cz/filmtit/share/TranslationPair.java
@@ -1,3 +1,20 @@
+/*Copyright 2012 FilmTit authors - Karel Bílek, Josef Čech, Joachim Daiber, Jindřich Libovický, Rudolf Rosa, Jan Václ
+
+This file is part of FilmTit.
+
+FilmTit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2.0 of the License, or
+(at your option) any later version.
+
+FilmTit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FilmTit.  If not, see <http://www.gnu.org/licenses/>.*/
+
 package cz.filmtit.share;
 
 import org.hibernate.annotations.Type;
